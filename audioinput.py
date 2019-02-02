222a31ae1070dc84c831cf56a70b51943fa55599
Nathan <npapapietro@crimson.ua.edu>
2019-02-02 14:23:43 -0600

    rearranging

--- audioinput.py
+++
@@ -1,9 +0,0 @@
-from scipy.io import wavfile
-
-
-
-hello = ["hello{}.wav".format(x+1) for x in range (3)]
-gbye = ["goodbye{}.wav".format(x+1) for x in range (3)]
-
-sf, snd = wavfile.read("CatScratch/"+hello[0])
-print(sf)
